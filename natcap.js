8af051545e828edea0522c5ebb21af4ff6956591
Doga Tekin <doga.tekin@epfl.ch>
2018-11-19 02:11:42 +0100

    Use d3-geomap

--- natcap.js
+++ natcap.js
@@ -1,29 +1,18 @@
-//Width and height
-var w = 1280;
-var h = 720;
+var format = function(d) {
+    d = d / 1000000;
+    return d3.format(',.02f')(d) + 'M';
+}
 
-var projection = d3.geoMercator()
-                   .translate([w/2, h/2])
-                   .scale(100)
+var map = d3.geomap.choropleth()
+    .geofile('/d3-geomap/topojson/world/countries.json')
+    .colors(colorbrewer.YlGnBu[9])
+    .column('2010')
+    .format(format)
+    .legend(true)
+    .unitId('iso3');
 
-//Define path generator, using the Albers USA projection
-var path = d3.geoPath(projection)
-       // .projection(d3.geoAlbersUsa());
-
-//Create SVG element
-var svg = d3.select("body")
-      .append("svg")
-      .attr("width", w)
-      .attr("height", h);
-
-//Load in GeoJSON data
-d3.json("https://unpkg.com/world-atlas@1/world/110m.json").then( function(json) {
-
-  //Bind data and create one path per GeoJSON feature
-  svg.selectAll("path")
-     .data(topojson.feature(json, json.objects.countries).features)
-     .enter()
-     .append("path")
-     .attr("d", path);
-
+d3.csv('sp.pop.totl.csv', function(error, data) {
+    console.log(data)
+    var selection = d3.select('#map').datum(data);
+    map.draw(selection);
 });
