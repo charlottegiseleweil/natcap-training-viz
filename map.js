1c788f69d02729a52c25bfd7c1d520df8024899d
Doga Tekin <doga.tekin@epfl.ch>
2018-12-09 22:32:45 +0100

    Add year slider and toggler

--- map.js
+++ map.js
@@ -67,4 +67,42 @@ $.getJSON("Data/data.json", function(data) {
       },
     });
   });
+
+  var val = 2018
+  var handle = $( "#custom-handle" );
+
+  $("#slider").slider({
+    value: val,
+    min: 2013,
+    max: 2018,
+    step: 1,
+    disabled: true,
+    create: function() {
+      handle.text( $( this ).slider( "value" ) );
+    },
+    slide: function(event, ui) {
+      val = ui.value;
+      console.log(val)
+      handle.text( val );
+      //TODO: Update Map and Charts
+    }
+  });
+
+  $( "input" ).checkboxradio({
+    icon: false,
+  });
+
+  function handleToggle( e ) {
+    var target = $( e.target );
+    if (target.is( ":checked" )) {
+      $( "#slider" ).slider( "enable" );
+      //TODO: Update Map and Charts
+    }
+    else {
+      $( "#slider" ).slider( "disable" );
+      //TODO: Update Map and Charts
+    }
+  }
+
+  $( "#checkbox-1" ).on( "change", handleToggle );
 });
