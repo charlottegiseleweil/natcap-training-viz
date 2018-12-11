3140587282dc519330878717b65fe7361a6121e9
Doga Tekin <doga.tekin@epfl.ch>
2018-12-11 11:51:50 +0100

    Make line chart responsive

--- line-chart.js
+++ line-chart.js
@@ -10,113 +10,134 @@ d3.csv("Data/chart-data.csv", (d, i, columns) => {
     dates: d.columns.slice(1).map(d3.timeParse("%Y"))
   }
 
-  size = d3.select('svg').node().parentNode.getBoundingClientRect()
+  console.log(data)
 
-  width = size.width
-  height = size.height
+  function drawGraphic() {
+    size = d3.select('#line-chart').node().parentNode.getBoundingClientRect()
 
-  margin = ({top: 20, right: 20, bottom: 30, left: 40})
+    width = size.width
+    height = size.height
 
-  x = d3.scaleTime()
-    .domain(d3.extent(data.dates))
-    .range([margin.left, width - margin.right])
+    margin = ({top: 20, right: 20, bottom: 30, left: 40})
 
-  y = d3.scaleLinear()
-    .domain([0, d3.max(data.series, d => d3.max(d.values))]).nice()
-    .range([height - margin.bottom, margin.top])
+    x = d3.scaleTime()
+      .domain(d3.extent(data.dates))
+      .range([margin.left, width - margin.right])
 
-  line = d3.line()
-    .defined(d => !isNaN(d))
-    .x((d, i) => x(data.dates[i]))
-    .y(d => y(d))
+    y = d3.scaleLinear()
+      .domain([0, d3.max(data.series, d => d3.max(d.values))]).nice()
+      .range([height - margin.bottom, margin.top])
 
-  xAxis = g => g
-    .attr("transform", `translate(0,${height - margin.bottom})`)
-    .call(d3.axisBottom(x).ticks(width / 80).tickSizeOuter(0))
+    line = d3.line()
+      .defined(d => !isNaN(d))
+      .x((d, i) => x(data.dates[i]))
+      .y(d => y(d))
 
-  yAxis = g => g
-    .attr("transform", `translate(${margin.left},0)`)
-    .call(d3.axisLeft(y))
-    .call(g => g.select(".domain").remove())
-    .call(g => g.select(".tick:last-of-type text").clone()
-        .attr("x", 3)
-        .attr("text-anchor", "start")
-        .attr("font-weight", "bold")
-        .text(data.y))
+    xAxis = g => g
+      .attr("transform", `translate(0,${height - margin.bottom})`)
+      .call(d3.axisBottom(x).ticks(width / 80).tickSizeOuter(0))
 
-  // const svg = d3.select(DOM.svg(width, height));
-  const svg = d3.select('svg')
-                .attr('height', height)
-                .attr('width', width)
+    yAxis = g => g
+      .attr("transform", `translate(${margin.left},0)`)
+      .call(d3.axisLeft(y))
+      .call(g => g.select(".domain").remove())
+      .call(g => g.select(".tick:last-of-type text").clone()
+          .attr("x", 3)
+          .attr("text-anchor", "start")
+          .attr("font-weight", "bold")
+          .text(data.y))
 
-  svg.append("g")
-      .call(xAxis);
+    // const svg = d3.select(DOM.svg(width, height));
+    const svg = d3.select('#line-chart')
+                  .attr('height', height)
+                  .attr('width', width)
 
-  svg.append("g")
-      .call(yAxis);
+    svg.append("g")
+        .call(xAxis);
 
-  const path = svg.append("g")
-      .attr("fill", "none")
-      .attr("stroke", "steelblue")
-      .attr("stroke-width", 1.5)
-      .attr("stroke-linejoin", "round")
-      .attr("stroke-linecap", "round")
-    .selectAll("path")
-    .data(data.series)
-    .enter().append("path")
-      .style("mix-blend-mode", "multiply")
-      .attr("d", d => line(d.values));
+    svg.append("g")
+        .call(yAxis);
 
-  svg.call(hover, path);
-  svg.call(responsivefy);
+    const path = svg.append("g")
+        .attr("fill", "none")
+        .attr("stroke", "steelblue")
+        .attr("stroke-width", 1.5)
+        .attr("stroke-linejoin", "round")
+        .attr("stroke-linecap", "round")
+      .selectAll("path")
+      .data(data.series)
+      .enter().append("path")
+        .style("mix-blend-mode", "multiply")
+        .attr("d", d => line(d.values));
 
-  function hover(svg, path) {
-    svg
-        .style("position", "relative");
+    svg.call(hover, path);
+    // svg.call(responsivefy);
 
-    if ("ontouchstart" in document) svg
-        .style("-webkit-tap-highlight-color", "transparent")
-        .on("touchmove", moved)
-        .on("touchstart", entered)
-        .on("touchend", left)
-    else svg
-        .on("mousemove", moved)
-        .on("mouseenter", entered)
-        .on("mouseleave", left);
+    function hover(svg, path) {
+      svg
+          .style("position", "relative");
 
-    const dot = svg.append("g")
-        .attr("display", "none");
+      if ("ontouchstart" in document) svg
+          .style("-webkit-tap-highlight-color", "transparent")
+          .on("touchmove", moved)
+          .on("touchstart", entered)
+          .on("touchend", left)
+      else svg
+          .on("mousemove", moved)
+          .on("mouseenter", entered)
+          .on("mouseleave", left);
 
-    dot.append("circle")
-        .attr("r", 2.5);
+      const dot = svg.append("g")
+          .attr("display", "none");
 
-    dot.append("text")
-        .style("font", "10px sans-serif")
-        .attr("text-anchor", "middle")
-        .attr("y", -8);
+      dot.append("circle")
+          .attr("r", 2.5);
 
-    function moved() {
-      d3.event.preventDefault();
-      const ym = y.invert(d3.event.layerY);
-      const xm = x.invert(d3.event.layerX);
-      const i1 = d3.bisectLeft(data.dates, xm, 1);
-      const i0 = i1 - 1;
-      const i = xm - data.dates[i0] > data.dates[i1] - xm ? i1 : i0;
-      const s = data.series.reduce((a, b) => Math.abs(a.values[i] - ym) < Math.abs(b.values[i] - ym) ? a : b);
-      path.attr("stroke", d => d === s ? null : "#ddd").filter(d => d === s).raise();
-      dot.attr("transform", `translate(${x(data.dates[i])},${y(s.values[i])})`);
-      dot.select("text").text(s.name);
+      dot.append("text")
+          .style("font", "10px sans-serif")
+          .attr("text-anchor", "middle")
+          .attr("y", -8);
+
+      function moved() {
+        d3.event.preventDefault();
+        const ym = y.invert(d3.event.layerY);
+        const xm = x.invert(d3.event.layerX);
+        const i1 = d3.bisectLeft(data.dates, xm, 1);
+        const i0 = i1 - 1;
+        const i = xm - data.dates[i0] > data.dates[i1] - xm ? i1 : i0;
+        const s = data.series.reduce((a, b) => Math.abs(a.values[i] - ym) < Math.abs(b.values[i] - ym) ? a : b);
+        path.attr("stroke", d => d === s ? null : "#ddd").filter(d => d === s).raise();
+        dot.attr("transform", `translate(${x(data.dates[i])},${y(s.values[i])})`);
+        dot.select("text").text(s.name);
+      }
+
+      function entered() {
+        path.style("mix-blend-mode", null).attr("stroke", "#ddd");
+        dot.attr("display", null);
+      }
+
+      function left() {
+        path.style("mix-blend-mode", "multiply").attr("stroke", null);
+        dot.attr("display", "none");
+      }
     }
+  }
 
-    function entered() {
-      path.style("mix-blend-mode", null).attr("stroke", "#ddd");
-      dot.attr("display", null);
-    }
+  drawGraphic()
 
-    function left() {
-      path.style("mix-blend-mode", "multiply").attr("stroke", null);
-      dot.attr("display", "none");
-    }
+  var resizeTimer;
+  window.onresize = function(event) {
+   clearTimeout(resizeTimer);
+    resizeTimer = setTimeout(function()
+    {
+      var s = d3.select('#line-chart');
+      var parent = s.node().parentNode
+      s = s.remove();
+      // console.log(parent)
+      d3.select(parent).append('svg').attr('id', 'line-chart')
+      // d3.select("body").append("svg")
+      drawGraphic();
+    }, 50);
   }
 
   function responsivefy(svg) {
@@ -144,12 +165,10 @@ d3.csv("Data/chart-data.csv", (d, i, columns) => {
         svg.attr("width", targetWidth);
         svg.attr("height", Math.round(targetWidth / aspect));
 
-        // x = d3.scaleTime()
-        //   .domain(d3.extent(data.dates))
-        //   .range([margin.left, targetWidth - margin.right])
-        // y = d3.scaleLinear()
-        //   .domain([0, d3.max(data.series, d => d3.max(d.values))]).nice()
-        //   .range([Math.round(targetWidth / aspect) - margin.bottom, margin.top])
+        // x.range([margin.left, targetWidth - margin.right])
+        // y.range([Math.round(targetWidth / aspect) - margin.bottom, margin.top])
+        // svg.call(hover, path);
+
     }
   }
 })
