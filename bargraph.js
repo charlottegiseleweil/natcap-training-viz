036498b30ce550301f4709fa51ac8e068bc60b34
sdhar3 <shikhardhar12345@gmail.com>
2018-12-18 14:45:08 +0100

    changed filter by country

--- bargraph.js
+++ bargraph.js
@@ -41,27 +41,60 @@ function createGraph_bar(
         };
       }
     } else {
-      var filtered_data = d.filter(function(x) {
-        return x.d.Country === country && x.d.Year === year;
-      });
+      if (year === "Total") {
+        var filtered_data = d.filter(function(x) {
+          return x.d.Country === country && x.d.Year === year;
+        });
 
-      years = [];
-      values = [];
-      for (var i = 0; i < filtered_data.length; i++) {
-        years.push(filtered_data[i].d.Type);
-        values.push(parseInt(filtered_data[i].d.Count));
+        data = {
+          Year: ["2013", "2014", "2015", "2016", "2017", "2018"],
+          Intro: [],
+          Nodal: [],
+          Partner: [],
+          Public: [],
+          Regional: [],
+          Seminar: [],
+          Symposium: [],
+          Webinar: []
+        };
+
+        for (var i = 0; i < data.Year.length; i++) {
+          var filtered_data = d.filter(function(x) {
+            return x.d.Country === country && x.d.Year === data.Year[i];
+          });
+
+          data.Intro.push(filtered_data[0].d.Count);
+          data.Nodal.push(filtered_data[1].d.Count);
+          data.Partner.push(filtered_data[2].d.Count);
+          data.Public.push(filtered_data[3].d.Count);
+          data.Regional.push(filtered_data[4].d.Count);
+          data.Seminar.push(filtered_data[5].d.Count);
+          data.Symposium.push(filtered_data[6].d.Count);
+          data.Webinar.push(filtered_data[7].d.Count);
+        }
+      } else {
+        var filtered_data = d.filter(function(x) {
+          return x.d.Country === country && x.d.Year === year;
+        });
+
+        years = [];
+        values = [];
+        for (var i = 0; i < filtered_data.length; i++) {
+          years.push(filtered_data[i].d.Type);
+          values.push(parseInt(filtered_data[i].d.Count));
+        }
+
+        data = {
+          Year: years,
+          Trainees: values
+        };
       }
-
-      data = {
-        Year: years,
-        Trainees: values
-      };
     }
 
     function drawGraph(class_data, type, country, year) {
       var transformedData = {};
 
-      if (country === "WORLD" && year === "Total") {
+      if (year === "Total") {
         if (type) {
           transformedData = class_data.Year.map((Year, index) => ({
             Year,
@@ -105,7 +138,7 @@ function createGraph_bar(
 
       var widthStackChart = 0;
 
-      if (country === "WORLD" && year === "Total") {
+      if (year === "Total") {
         widthStackChart =
           width - marginStackChart.left - marginStackChart.right - 60;
       } else {
@@ -279,7 +312,7 @@ function createGraph_bar(
         });
 
       var variable = 30;
-      if (country === "WORLD" && year === "Total") {
+      if (year === "Total") {
         variable = 60;
       } // else {
       //   variable
